6e21345839839775d875a5b73774afc4ff321369
Ayoub Amzil <aamzil@e2r10p6.1337.ma>
2021-03-09 12:57:19 +0100

    yaml for wordpress

--- mysql/script/install.sh
+++ mysql/script/install.sh
@@ -6,11 +6,6 @@ touch /run/openrc/softlevel
 rc-status
 apk update
 apk upgrade
-apk add nginx
-adduser -D -g 'www' www
-mkdir /www
-chown -R www:www /var/lib/nginx
-chown -R www:www /www
 apk add mysql mysql-client
 /etc/init.d/mariadb setup
-echo test > /www/index.html
+service mariadb start
--- mysql/script/start.sh
+++ mysql/script/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-service nginx start
 service mariadb start
+mariadb -u root -e "FLUSH PRIVILEGES;ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
